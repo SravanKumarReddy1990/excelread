62ca847c2b213c793b97237e97a9cd7e5612979b
SravanKumarReddy1990 <bskreddy1990@gmail.com>
2019-10-14 17:17:44 +0530

    first commit

---
+++ src/main/java/com/excel/Sample/Servlet/FileImage.java
@@ -0,0 +1,126 @@
+package com.excel.Sample.Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.excel.Sample.Actions.ExcelAction;
+import com.excel.Sample.Actions.ExcelActionXssf;
+import com.excel.Sample.Model.Directory;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ * Servlet implementation class ExcelSample
+ */
+public class FileImage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FileImage() {
+		super();
+
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		try {
+
+			boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+			if (!isMultipart) {
+			} else {
+				FileItemFactory factory = new DiskFileItemFactory();
+				ServletFileUpload upload = new ServletFileUpload(factory);
+				List items = null;
+				try {
+					items = upload.parseRequest(request);
+				} catch (FileUploadException e) {
+					e.getMessage();
+				}
+				FileItem item = (FileItem) items.get(0);
+				try {
+long l=(long)Math.random()*1000000000000L;
+
+					 String url = "jdbc:postgresql://ec2-23-21-160-80.compute-1.amazonaws.com:5432/d4ovlnqvutd1j7";
+            Connection conn = 							DriverManager.getConnection(url,"ckimwlfkyjkcvd","8a898408823185c78744e7bd54d71c87a4b0953ccd7271657265386796e24cbd");
+   Statement statement = connection.createStatement();
+   PreparedStatement ps = connection.prepareStatement("INSERT INTO images VALUES (?, ?, ?)");
+   ps.setInt(1, l+"");
+   ps.setBytes(2, item.getName());
+   ps.setString(3, readFully(item.getInputStream()));
+   ps.executeUpdate();
+   ps.close();
+		out.println("item.getName() : "+item.getName());			
+				} catch (Exception e) {
+					out.println("Excel Sample : "+e);
+				}
+			}
+		} catch (Exception e) {
+			out.println(e);
+		}
+
+	}
+public static byte[] readFully(InputStream input) throws IOException
+{
+    byte[] buffer = new byte[8192];
+    int bytesRead;
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    while ((bytesRead = input.read(buffer)) != -1)
+    {
+        output.write(buffer, 0, bytesRead);
+    }
+    return output.toByteArray();
+}
+
+	public static String readJsonFromUrl(String theUrl) {
+    StringBuilder content = new StringBuilder();
+    try
+    {
+      URL url = new URL(theUrl);
+      URLConnection urlConnection = url.openConnection();
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+      String line;
+      while ((line = bufferedReader.readLine()) != null)
+      {
+        content.append(line + "\n");
+      }
+      bufferedReader.close();
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+    return content.toString();
+	}
+}
