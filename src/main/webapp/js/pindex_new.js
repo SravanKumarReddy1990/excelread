370208643574ff903866f4cd7ea433f462d8a4c3
SravanKumarReddy1990 <bskreddy1990@gmail.com>
2020-01-30 09:45:56 +0530

    first

--- src/main/webapp/js/pindex_new.js
+++ src/main/webapp/js/pindex_new.js
@@ -54,8 +54,7 @@ if(received_msg_json.contentType.localeCompare("loc")==0){
 var contents=received_msg_json.content;
 var con=contents.split(",");
 var format = new ol.format.WKT();
-var feature = format.readFeature(
-    'POINT(('+con[0]+' '+con[1]+'))');
+var feature = format.readFeature('POINT(('+con[0]+' '+con[1]+'))');
 feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
 
 var vector_layer = new ol.layer.Vector({
