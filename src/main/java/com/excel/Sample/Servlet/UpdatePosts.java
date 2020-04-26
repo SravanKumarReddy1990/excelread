1d233a6228c756ddd30d151f510582ed87f8e33e
SravanKumarReddy1990 <bskreddy1990@gmail.com>
2020-04-26 18:11:26 +0530

    first commit

--- src/main/java/com/excel/Sample/Servlet/UpdatePosts.java
+++ src/main/java/com/excel/Sample/Servlet/UpdatePosts.java
@@ -24,13 +24,13 @@ import javax.servlet.http.*;
 /**
  * Servlet implementation class ExcelSample
  */
-public class Login extends HttpServlet {
+public class UpdatePosts extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public Login() {
+	public UpdatePosts() {
 		super();
 
 		// TODO Auto-generated constructor stubs
